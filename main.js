bbb20cb3ac7164d1913056153cee3cb4b7c8421c
ksquare41 <112753911+ksquare41@users.noreply.github.com>
2022-09-03 16:02:16 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,16 +12,14 @@ class Calculator{
     }
     delete(){
         this.currentOperand = this.currentOperand.slice(0, -1)
-
     }
     appendNumber(number){
         if(number === '.' && this.currentOperand.includes('.')) return
-        if (this.chackEqual == 1){
+        if (this.chackEqual == 1 && this.currentOperand != 0){
             this.clear()
             this.chackEqual = 0
         }
         this.currentOperand = this.currentOperand.toString() + number.toString()
-
     }
     chooseOperation(operation){
         if(this.currentOperand === '') return
@@ -31,7 +29,6 @@ class Calculator{
         this.operation = operation
         this.prevOperand = this.currentOperand
         this.currentOperand = ''
-
     }
     compute(){
         let computation
@@ -50,6 +47,7 @@ class Calculator{
             default:
                 return
         }
+        this.createHistory(prev, current, this.operation, computation)
         this.currentOperand = computation
         this.operation = undefined
         this.prevOperand = ''
@@ -81,12 +79,25 @@ class Calculator{
             this.prevText.innerText = ''
         }
     }
-    clickSound(audio){
-        var audio = audio; 
-        audio. play();
+    createHistory(prev, current, operation, computation){
+        const historyTable = document.getElementById('historyTable')
+        historyTable.style.width = '20em'
+        toggleId = 0
+        const history = document.createElement('div')
+        history.classList.add('history-list')
+        historyTable.appendChild(history)
+        const equation = document.createElement('span')
+        equation.classList.add('operation-equation')
+        equation.innerText = prev.toString() + operation.toString() + current.toString()
+        const value = document.createElement('span')
+        value.classList.add('operation-value')
+        value.innerText = computation.toString()
+        history.appendChild(equation)
+        history.appendChild(value)
     }
 }
 
+let toggleId = 1
 
 const numberButton = document.querySelectorAll('[data-number]')
 const operationButton = document.querySelectorAll('[data-operation]')
@@ -97,48 +108,58 @@ const prevText = document.querySelector("[data-prev]")
 const currentText = document.querySelector("[data-current]")
 
 
+
+
 const calculator = new Calculator(prevText, currentText)
 
 numberButton.forEach(button => {
     button.addEventListener('click', ()=>{
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
-        calculator.clickSound()
     })
 })
 operationButton.forEach(button => {
     button.addEventListener('click', ()=>{
         calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
-        calculator.clickSound()
         calculator.chackEqual = 0
     })
 })
 equalsButton.addEventListener('click', button =>{
     calculator.compute()
     calculator.updateDisplay()
-    var audio = new Audio("compute.mp3");
-    calculator.clickSound(audio)
     calculator.chackEqual = 1
 })
 allClearButton.addEventListener('click', button =>{
     calculator.clear()
     calculator.updateDisplay()
-    calculator.clickSound()
 })
 deleteButton.addEventListener('click', button =>{
     calculator.delete()
     calculator.updateDisplay()
-    calculator.clickSound()
 })
 
 
+function clearHistory(){
+    const historyTable = document.getElementById('historyTable')
+    historyTable.style.width = 0
+    toggleId = 0
+    const histories = document.querySelectorAll('.history-list')
+    histories.forEach(history => {
+        history.remove();
+    });
+}
 
-
-
-
-
-
+function toggleHistory(){
+    const historyTable = document.getElementById('historyTable')
+    if(toggleId == 1){
+        historyTable.style.width = '20em'
+        toggleId = 0
+    }else{
+        historyTable.style.width = 0
+        toggleId = 1
+    }
+}
 
 
 
